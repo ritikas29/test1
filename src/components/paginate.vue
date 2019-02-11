c3e367464b598530cb79bf1c44524cb4eef84713
ritika29 <setritika16@gmail.com>
2019-02-11 15:39:27 +0530

    First merge

---
+++ src/components/paginate.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="container">
+   <table class="table table-striped mt-4">
+  <thead>
+    <tr>
+      <th scope="col">AlbumID</th>
+    <th scope="col">ID</th>
+      <th scope="col">Title</th>
+      <th scope="col">url</th>
+      <th scope="col">thumbnailUrl</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="paginate in paginate" :key="paginate.id" >
+     
+      <th scope="row">1</th>
+      <td>{{paginate.id}}</td>
+      <td>{{paginate.albumid}}</td>
+      <td>{{paginate.title}}</td>
+      <td>{{paginate.url}}</td>
+      <td>{{paginate.thubnailUrl}}</td>
+
+    </tr>
+    
+  </tbody>
+  <b-row>
+      <b-col md="6" class="table table-striped mt-4">
+        <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0" />
+      </b-col>
+    </b-row>
+   <div>currentPage: {{currentPage}}</div>
+</table>
+</div>
+
+</template>
+<script>
+import axios from 'axios'
+import bPagination from 'bootstrap-vue/es/components/pagination/pagination';
+export default {
+  name:'paginate',
+  components:
+  'b-pagination',
+  data() {
+    return {
+      paginate:[],
+       currentPage: 1,
+       perPage:5,
+       totalRows:5,
+       pageOptions: [5,10,15],
+      }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/photos').then(paginate => {
+    console.log(paginate.data);
+    this.paginate= paginate.data
+    });
+  }
+}
+</script>
--- src/views/secure.vue
+++ src/views/secure.vue
@@ -1,30 +1,91 @@
 <template>
     <div id="secure">
-
-    <div class="file btn btn-lg btn-primary">
+      
+    <!--div class="file btn btn-lg btn-primary">
    <button type="button" class="btn btn-lg btn-primary" disabled>upload</button>
    <input type="file" name="file"/>
-   </div>
-    <button type="button" class="btn btn-secondary" v-on:click="submit()" >SUBMIT</button>
+   </>
+    <button type="button" class="btn btn-secondary" v-on:click="submit()" >SUBMIT</button  -->
+    <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>Files
+        <input type="file" id="files" ref="files" />
+      </label>
+      <button type="button" class="btn btn-secondary" v-on:click="submitFiles()" >SUBMIT</button>
+      <button type="button" class="btn btn-secondary" v-on:click="showData()" >Result</button>
+      <div v-if="result" >
+          <paginate></paginate>
+      </div>
+      
+            <!--router-link to="/secure">result
+
+      <paginate></paginate>
+      </router-link-->
+    
+      <!--router-link to="/paginate" v-on:click.native="submitFiles()" replace>
+            <button type="button" class="btn btn-primary">submit</button>
+       </router-link-->
     </div>
+    
+  </div>
+</div>
 </template>
 
 <script>
+//import axios from "axios"
+import paginate from "../components/paginate.vue"
+
     export default {
+        
         name: 'Secure',
+        components:{ 
+            Paginate: paginate
+        
+          },
         data() {
-            return {};
+            return {
+                result : false,
+                showData(){
+                    this.result = true;
+                    alert('sucessfully 1');
+                },
+            }
         },
         methods: {
-            submit() {
+
+
+
+            submitFiles() {
                 alert('sucessfully ')
       // `event` is the native DOM event
          if (event) {
         alert(event.target.tagName)
             }
           }
-        }
-    }
+
+
+               /*let formData = new FormData();
+               for(var i=0;i<this.files.length;i++) {
+                   let file = this.files[i]
+                   formData.append('files['+i +']',file)
+               }
+                axios.post('http://localhost:3000/posts',
+                formData,
+                {
+                    headers: {
+                        'content-type':'multipart/form-data'
+                    }
+                } 
+                ).then(function(){
+                    console.log('success')
+                })
+                .catch(function(){
+                    console.log("failure !!")
+                });*/
+          }
+          
+     }
+
 </script>
 
 <style  scoped>
