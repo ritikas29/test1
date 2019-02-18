c59115626fce45cfa0df3db1d893383ae16f73f9
sakshi2030 <ss20sakshi@gmail.com>
2019-02-18 13:34:27 +0530

    modified thumbnail page

---
+++ src/components/paginate.vue
@@ -0,0 +1,83 @@
+<template>
+
+   <div class="container">
+  <table class="table table-striped mt-4">
+  <thead>
+    
+    <th scope="col">sno</th>
+    <th scope="col">images</th>
+    <th scope="col">trashfound</th>
+    <th scope="col">confidence</th>
+    <th scope="col">modify</th>
+  </thead>
+  <tbody>
+    <tr v-for="paginate in paginate" :key="paginate.id" >
+    <!--  <th scope="row"></th> -->
+
+      <td>{{paginate.sno}}</td>
+      <td><img :src=" paginate.images "   width="100" height="100"/> 
+    </td>
+      <td>{{paginate.trashfound}}</td> 
+       <td> {{paginate.confidence}}</td>
+      <td>{{paginate.modify}}</td>
+    </tr>
+    
+  </tbody>
+
+</table>
+
+       </div> 
+</template>
+<script>
+//import json1 from '../assets/json/table.json';
+import axios from 'axios' //local directory
+export default{
+  
+    data(){
+        return {
+            paginate: [] 
+        }
+    },
+    created(){
+        //console.log(json1);
+
+                
+         axios.get('http://localhost:3000/comments').then(paginate =>{
+              
+              
+               this.paginate =paginate.data
+
+         })
+         //.catch(error => console.log(error));
+
+        /*axios.get('https://jsonplaceholder.typicode.com/todos/1').then(posts =>{
+        
+          console.log(posts.data);
+              
+           this.posts =posts.data
+
+         });*/
+    } 
+}         
+ </script>
+ <style>
+
+  table,thead,tbody
+        {
+            /*background-color: #aaa;*/
+            width: 100px;
+        }
+        tbody
+        {
+            /*background-color: #ddd;*/
+         overflow: auto;
+        }
+       
+      /*  thead > tr, tbody
+        {
+            display: block;
+        } */
+   </style>  
+
+        
+    
--- src/views/secure.vue
+++ src/views/secure.vue
@@ -1,12 +1,24 @@
 <template>
     <div id="secure">
 
-    <div class="file btn btn-lg btn-primary"><br/>
+    <div class="file btn btn-lg btn-primary">
   <!-- <button type="button" class="btn btn-lg btn-primary" disabled>Upload</button><br/>-->
-   <input type="file" name="file"/>
-   </div>
-   <br/>
-    <button type="button" class="btn btn-secondary" v-on:click="submit()" >SUBMIT</button>
+   <!--input type="file" @change="file"/>
+       <button type="button" class="btn btn-secondary" v-on:click="submitFiles()" >SUBMIT</button-->
+    <!--input type="file" v-on:change="onFileChanged"-->
+  <label>Files
+        <input type="file" id="files" ref="files" multiple v-on:change="handleFilesUpload()"/>
+      </label>
+    </div>
+    <br/>
+
+      <button v-on:click="submitFiles()">Submit</button>
+   
+   
+    <button type="button" class="btn btn-secondary" v-on:click="showData()" >Result</button>
+      <div v-if="result" >
+          <paginate></paginate>
+
     </div>
 </template>
 
@@ -35,17 +47,32 @@
    #secure {
        position: relative;
         /*background: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQKpmxOUMC8yHZrzWTow6KvUeuOHN13Cdq5rJ2kZZA5vKKCKKzew); */
+      /*  background-color: #FFFFFF; */
+       /* padding: 200px; */
         margin-top: 10px;
-    } 
+        border-color: #504d4c;
+       
+    }
+    .btn-lg {
+    padding: .5rem 1rem;
+    font-size: 0.9rem;
+    line-height: 1.5;
+    border-radius: .3rem;
+    position: absolute;
+    top: 50px;
+} 
     
 button{
-    background:lightgreen; 
-    color: #6373cc;
+    background:#99db49; 
+    color: #08176f;
     padding: 12px 12px;
     border: none;
+    vertical-align: top;
     border-radius: 4px;
     cursor: pointer;
-    margin-top:20px;
+    margin-left:20px;
+    top: 70%;
+    left: 50%;
 }
 
 button:hover{
